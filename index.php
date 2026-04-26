9414dacde0023d57c4aab764f40118290153b435
David Hernandez <david.92hdez@gmail.com>
2026-04-26 18:52:47 +0100

    Carpeta subida en el despliegue. No pasar a Main

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
+
 //NOTA DE PAULA: Tomo de referencia la estructura que aprendí para EduFlow, me parece que deja un index bastante limpio sin que la primera página sea una vista y centraliza todo a través del index.
 
 // Definimos una constante BASE_URL para usarla en las redirecciones y enlaces, así evitamos problemas con rutas relativas
-if(!defined('BASE_URL')) {
+if (!defined('BASE_URL')) {
     define('BASE_URL', '/Fitmemory/');
 }
 // Requerimos las funciones y clases
@@ -23,13 +24,13 @@ if (!isset($_GET['vista'])) {
     // Si NO está definido, comprobamos si el parámetro público de sesión 'usuario' está definido
     if (isset($_SESSION['perfil'])) {
         // Si 'perfil' está definido, comprobamos el tipo de usuario y lo enviamos a su correspondiente dashboard
-        if ($_SESSION['perfil']=="administrador") {
+        if ($_SESSION['perfil'] == "administrador") {
             header('Location: ' . BASE_URL . 'index.php?vista=adminDashboard');
         }
-        if ($_SESSION['perfil']=="cliente") {
+        if ($_SESSION['perfil'] == "cliente") {
             header('Location: ' . BASE_URL . 'index.php?vista=clienteDashboard');
         }
-        if ($_SESSION['perfil']=="entrenador") {
+        if ($_SESSION['perfil'] == "entrenador") {
             header('Location: ' . BASE_URL . 'index.php?vista=entrenadorDashboard');
         }
     } else {
@@ -46,5 +47,3 @@ if (!isset($_GET['vista'])) {
 
 // Llamamos a la función del controlador que enruta según el valor de vista
 enrute();
-
-?>
